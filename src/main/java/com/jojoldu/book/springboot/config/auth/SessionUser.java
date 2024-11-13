d48b602a7000190516f6bc2831138af139dfb33c
Kim Chanho <beroso20th@gmail.com>
2024-11-13 21:36:31 +0900

    refactor: SerializationFailedException해결

--- src/main/java/com/jojoldu/book/springboot/config/auth/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SessionUser.java
@@ -3,8 +3,12 @@ package com.jojoldu.book.springboot.config.auth;
 import com.jojoldu.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String name;
     private String email;
     private String picture;
